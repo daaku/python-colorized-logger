82713b89de0d33736fede8b458fd1a5025fcae0c
Naitik Shah <naitik.shah@yahoo.com>
2008-11-15 02:48:09 -0800

    haha - never worked, copy paste doesnt mix with binary data

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 import logging
 
 
-DEF_COLOR="^[[0m"
-CYAN="^[[36;01m"
-GREEN="^[[32;01m"
-YELLOW="^[[33;01m"
-RED="^[[31;01m"
+DEF_COLOR="[0m"
+BLUE="[34;01m"
+CYAN="[36;01m"
+GREEN="[32;01m"
+RED="[31;01m"
+GRAY="[37;01m"
+YELLOW="[33;01m"
 
 logging.addLevelName(logging.DEBUG,    CYAN   + '>> DEBUG')
 logging.addLevelName(logging.INFO,     GREEN  + '>> INFO')
